2defb771a4f9b557dd85ac355915e09e85db306a
Erno Hentonen <hentonen@gmail.com>
2019-08-12 13:23:57 +0300

    login reducer

---
+++ react/shoppinglist5/frontend/src/App.js
@@ -0,0 +1,157 @@
+import React from 'react';
+import './App.css';
+import MyForm from './MyForm';
+import MyList from './MyList';
+import {Switch,Route,Redirect,withRouter} from 'react-router-dom';
+import Navbar from './Navbar';
+import LoginForm from './LoginForm';
+import {connect} from 'react-redux';
+
+class App extends React.Component{
+  
+  constructor(props) {
+	  super(props);
+	  this.state= {
+			list:[]
+	  }
+  }
+  
+  componentDidMount() {
+	  if(this.props.isLogged) {
+		  this.getShoppingList();
+	  }
+  }
+
+  //SHOPPING API
+  
+  getShoppingList = () => {
+	  let request = {
+		  method:"GET",
+		  mode:"cors",
+		  headers:{"Content-Type":"application/json",
+					"token":this.props.token}
+	  }
+	  fetch("/api/shopping",request).then(response => {
+			if(response.ok) {
+				response.json().then(data => {
+					this.setState({
+						list:data
+					})
+				}).catch(error => {
+					console.log("Error in parsing response json");	
+				});
+			} else {
+				console.log("Server responded with status:"+response.statusText);
+
+	  }}).catch(error => {
+			console.log(error);
+		});
+  }
+  
+  addToList = (item) => {
+	  let request = {
+		  method:"POST",
+		  mode:"cors",
+		  headers:{"Content-Type":"application/json",
+					"token":this.props.token},
+		  body:JSON.stringify(item)
+	  }
+	  fetch("/api/shopping",request).then(response => {
+			if(response.ok) {
+				console.log("addToList success");
+				this.getShoppingList();
+				this.props.history.push("/list");
+			} else {
+				console.log("Server responded with status:"
+				+response.statusText);
+
+			}
+	  }).catch(error => {
+			console.log(error);
+	  })
+
+  }
+  
+  removeFromList = (id) => {
+	  let request = {
+		  method:"DELETE",
+		  mode:"cors",
+		  headers:{"Content-Type":"application/json",
+					"token":this.props.token}
+	  }
+	  fetch("/api/shopping/"+id,request).then(response => {
+			if(response.ok) {
+				console.log("RemoveFromList success");
+				this.getShoppingList();
+			} else {
+				console.log("Server responded with status:"+response.statusText);
+
+			}
+	  }).catch(error => {
+			console.log(error);
+	  })
+
+  }
+  
+  editItem = (item) => {
+	  let request = {
+		  method:"PUT",
+		  mode:"cors",
+		  headers:{"Content-Type":"application/json",
+					"token":this.props.token},
+		  body:JSON.stringify(item)
+	  }
+	  fetch("/api/shopping/"+item._id,request).then(response => {
+			if(response.ok) {
+				console.log("editItem success");
+				this.getShoppingList();
+			} else {
+				console.log("Server responded with status:"+response.statusText);
+
+			}
+	  }).catch(error => {
+			console.log(error);
+	  })
+
+  }
+  
+  render() {
+	  return (
+		<div className="App">
+			<Navbar	logout={this.logout}/>
+			<hr/>
+			<Switch>
+				<Route exact path="/" render={() => 
+					this.props.isLogged ?
+					(<Redirect to="/list"/>) :
+					(<LoginForm />)	
+				}/>
+				<Route path="/list" render={() => 
+					this.props.isLogged ?				
+					(<MyList list={this.state.list}
+							removeFromList={this.removeFromList}
+							editItem={this.editItem}/>) :
+					(<Redirect to="/"/>)
+				}/>
+				<Route path="/form" render={() => 
+					this.props.isLogged ?
+					(<MyForm addToList={this.addToList}/>) :
+					(<Redirect to="/"/>)
+				}/>
+				<Route render={() => 
+					(<Redirect to="/"/>)
+				}/>
+			</Switch>
+		</div>
+	  );
+  }
+}
+
+const mapStateToProps = (state) =>  {
+	return {
+		isLogged:state.isLogged,
+		token:state.token
+	}
+}
+
+export default withRouter(connect(mapStateToProps)(App));
---
+++ react/shoppinglist5/frontend/src/Navbar.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {List,Header} from 'semantic-ui-react';
+import {connect} from 'react-redux';
+import {onLogout} from './actions/loginActions';
+
+class Navbar extends React.Component {
+
+	logout = () => {
+		this.props.dispatch(onLogout(this.props.token));
+	}
+
+	render() {
+		let status = "Shoppinglist App"
+		if(this.props.loading) {
+			status = "Loading..."
+		}
+		if(this.props.error.length > 0) {
+			status = this.props.error
+		}
+		let navbar = <List></List>
+		if(this.props.isLogged) {
+			navbar = <List>
+					<List.Item><Link to="/list">Shopping List</Link></List.Item>
+					<List.Item><Link to="/form">Add To List</Link></List.Item>
+					<List.Item><Link to="/" onClick={this.logout}>Logout</Link></List.Item>
+				</List>
+		}
+		return(
+			<div style={{height:100, backgroundColor:"lightblue"}}>
+				<Header>{status}</Header>
+				{navbar}
+			</div>
+		)
+	}
+
+}
+
+const mapStateToProps = (state) => {
+	return {
+		isLogged:state.isLogged,
+		token:state.token,
+		loading:state.loading,
+		error:state.error
+	}
+}
+
+export default connect(mapStateToProps)(Navbar);
